3affa36a746b0b72b38349371219764d482ea0ec
ChanceDehar <chancedehar@hotmail.com>
2026-01-02 09:31:45 +1300

    problem 1 fix

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -2,10 +2,10 @@ class Solution:
     def twoSum(self, nums, target):
         previous = {}
         
-        for index, current_num in enumerate(nums):
-            needed_num = target - current_num
+        for i in range(len(nums)):
+            needed = target - nums[i]
             
-            if needed_num in previous:
-                return [previous[needed_num], index]
+            if needed in previous:
+                return [previous[needed], i]
             
-            previous[current_num] = index
+            previous[nums[i]] = i
